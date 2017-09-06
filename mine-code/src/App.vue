d8271e097d51c538715621333c73765b935c8361
micromi <484390775@qq.com>
2017-09-06 08:34:55 +0800

    add new case

---
+++ mine-code/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <div class="app-head">
+      <div class="app-head-inner">
+        <img src="./assets/logo.png">
+        <div class="head-nav">
+          <ul class="nav-list">
+            <li>登录</li>
+            <li class="nav-pile">|</li>
+            <li>注册</li>
+            <li class="nav-pile">|</li>
+            <li>关于</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="app-content">
+      <router-view></router-view>
+    </div>
+    <div class="app-foot">
+      © 2017 micromi MIT
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
+
+<style>
+
+</style>
